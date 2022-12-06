11892e5c567e9397f64a2b5d0aa5133f35b65f4c
Artem Medeu <artem.medeusheyev@gmail.com>
2022-12-06 12:31:07 +0600

    day6

--- src/core.clj
+++ src/core.clj
@@ -4,7 +4,8 @@
    [day2]
    [day3]
    [day4]
-   [day5]))
+   [day5]
+   [day6]))
 
 (time
  (mapv deref
@@ -12,4 +13,5 @@
         (future (day2/run))
         (future (day3/run))
         (future (day4/run))
-        (future (day5/run))]))
+        (future (day5/run))
+        (future (day6/run))]))
---
+++ src/day6.clj
@@ -0,0 +1,50 @@
+(ns day6
+  (:require [util :refer [->Result]]
+            [clojure.java.io :as io]))
+
+(defn input
+  []
+  (-> (io/resource "day6")
+      (slurp)))
+
+(defn find-distinct-distance
+  [in amount]
+  (reduce (fn [acc c]
+            (let [stack (conj acc c)
+                  cnt (count stack)
+                  tail (subvec stack (min cnt (abs (- cnt amount))))]
+              (if (= amount (count (distinct tail)))
+                (reduced (count stack))
+                stack)))
+          []
+          in))
+
+(subvec (persistent! (transient [1 2 3])) 1)
+(comment
+  (find-distinct-distance (input) 4)
+  (find-distinct-distance "bvwbjplbgvbhsrlpgdmjqwftvncz" 4)
+  (find-distinct-distance "nppdvjthqldpwncqszvftbrmjlhg" 4)
+  (find-distinct-distance "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" 4)
+  (find-distinct-distance "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" 4)
+  
+  (find-distinct-distance "mjqjpqmgbljsphdztnvjfqwrcgsmlb" 14)
+  (find-distinct-distance "bvwbjplbgvbhsrlpgdmjqwftvncz" 14)
+  (find-distinct-distance "nppdvjthqldpwncqszvftbrmjlhg" 14)
+  (find-distinct-distance "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" 14)
+  (find-distinct-distance "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" 14)
+  )
+
+(defn first-part
+  [in]
+  (find-distinct-distance in 4))
+
+(defn second-part
+  [in]
+  (find-distinct-distance in 14))
+
+(defn run []
+  (let [in (input)]
+    (->Result (first-part in) (second-part in))))
+
+(comment (time (run)))
+
